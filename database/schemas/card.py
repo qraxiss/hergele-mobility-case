53711eacb2e04d07bafa7cf3e10088f677be7514
qraxiss <qraxiss@gmail.com>
2023-04-29 20:36:24 +0300

    Create card.py

---
+++ database/schemas/card.py
@@ -0,0 +1,25 @@
+from database.schemas.schema import Schema
+allCardsSchema = {
+    'type': 'array',
+    'minItems': 0,
+}
+
+schema = {
+    "type": "object",
+    "properties": {
+        "userNo": {'type': 'number'},
+        "authCode": {'type': 'string'},
+        "name": {'type': 'string'},
+        "surname": {'type': 'string'},
+        "birthDate": {'type': 'string'},
+        "phoneNumber": {'type': 'number'},
+        "email": {'type': 'string'},
+        "selectedCard": {'type': 'string'},
+        "allCards": allCardsSchema,
+        "balance": {'type': 'number'},
+    },
+    "additionalProperties": False,
+    "required": []
+}
+
+card = Schema('card', schema)
